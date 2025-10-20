6218c83eda5819d026b14a5cb971bd0080a5c90f
samconde01 <sconde@samcondedigital.com>
2025-10-20 13:01:21 -0500

    Update stripe-webhook.js

--- pages/api/stripe-webhook.js
+++ pages/api/stripe-webhook.js
@@ -1,4 +1,4 @@
-// pages/api/stripe-webhook.js  (stub)
+// pages/api/stripe-webhook.js
 export default function handler(req, res) {
   return res.status(410).json({ error: 'Stripe is disabled for this project.' });
 }
