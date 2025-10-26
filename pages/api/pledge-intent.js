e40ea0127d9d4602af8e1391f9766b07d4aea9e3
samconde01 <sconde@samcondedigital.com>
2025-10-26 11:09:08 -0500

    Update pledge-intent.js

--- pages/api/pledge-intent.js
+++ pages/api/pledge-intent.js
@@ -1,4 +1,4 @@
-import { getSql } from "@/lib/db";
+import { getSql } from "../../lib/db";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
