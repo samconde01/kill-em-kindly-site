44c2bdde3d10880172715f2bcf99ec5a3d0eb5bf
samconde01 <sconde@samcondedigital.com>
2025-10-26 13:02:38 -0500

    Update ipn.js

--- pages/api/paypal/ipn.js
+++ pages/api/paypal/ipn.js
@@ -2,7 +2,7 @@
 export const config = { api: { bodyParser: false } };
 
 import getRawBody from "raw-body";
-import { getSql } from "../../../lib/db"; // relative path to /lib/db.js
+import { getSql } from "../../../lib/db";
 
 async function verifyIPN(raw) {
   const body = `cmd=_notify-validate&${raw.toString("utf8")}`;
