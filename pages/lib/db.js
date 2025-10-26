8bf42c00c3ea1cdce541f9dd726c65613db77632
samconde01 <sconde@samcondedigital.com>
2025-10-26 13:58:47 -0500

    Delete pages/lib directory

--- pages/lib/db.js
+++
@@ -1,7 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-
-export function getSql() {
-  const url = process.env.DATABASE_URL;
-  if (!url) throw new Error("DATABASE_URL is not set");
-  return neon(url);
-}
